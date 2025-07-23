0b351378d58baf4f1a72ab8b55b2eb13da66cbac
Aksheet Dutta <ardutta23@gmail.com>
2025-07-22 22:33:58 -0500

    finished everything a bit ago but forgot to push lol

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,42 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
-import User from "./models/user.model.js"; 
-import {connectDB} from "./lib/db.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
 const PORT = process.env.PORT;
-const app = express();
-app.use(express.json())
-app.use("/api/auth", authRoutes)
-app.listen(PORT, async () =>  {
-    console.log("server running on port " +  PORT);
-    connectDB();
-    const user = new User({
-        email: "tester@tsd,cdsdfdfin",
-        password: "fak222rk",
-      });
-    await user.save();
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
